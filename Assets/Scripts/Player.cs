a9efc0fc205bc2ed4fb014a4f79c6afa6f5287cd
qkrdmstn <qkrdmstn1258@naver.com>
2023-05-27 13:41:01 +0900

    애니메이션 추가, 대각선 이동 방지

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,24 +8,35 @@ public class Player : MonoBehaviour
     private Vector3 vector;
     private bool keyDown = false;
     int walkCount = 10;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     IEnumerator MoveCoroutine()
     {
-        verticalInput = Input.GetAxisRaw("Vertical");
-        horizonInput = Input.GetAxisRaw("Horizontal");
-        vector.Set(horizonInput * speed, verticalInput * speed, transform.position.z);
-
-        for(int i=0; i<walkCount; i++)
+       while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
-            transform.Translate(vector);
-            yield return new WaitForSeconds(0.01f);
+            verticalInput = Input.GetAxisRaw("Vertical");
+            horizonInput = Input.GetAxisRaw("Horizontal");
+            vector.Set(horizonInput * speed, verticalInput * speed, transform.position.z);
+
+            if (vector.x != 0)
+                vector.y = 0;
+         
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+
+            for (int i = 0; i < walkCount; i++)
+            {
+                transform.Translate(vector);
+                yield return new WaitForSeconds(0.01f);
+            }
         }
-            
+
+        animator.SetBool("Walking", false);
         keyDown = false;
     }
 
@@ -37,6 +48,7 @@ public class Player : MonoBehaviour
         {
             if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
             {
+                animator.SetBool("Walking", true);
                 keyDown = true;
                 StartCoroutine(MoveCoroutine());
             }
