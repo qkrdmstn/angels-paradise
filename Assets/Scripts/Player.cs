b24d529012dd2c02ff85a87b0df8b46ec3e5cdbb
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2023-06-23 00:30:53 +0900

    Revert "6/22 todo qkrdmstn"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,80 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.U2D.Animation;
+
 public class Player : MonoBehaviour
 {
-    private UIManager uiManager;
-    //Player Movment
     public float verticalInput, horizonInput;
     public float speed, runSpeed;
     private Vector3 vector;
     private bool keyDown = false;
     int walkCount = 10;
     private Animator animator;
-
-    //Change Character
-    private SpriteLibrary spriteLibrary;
-    public SpriteLibraryAsset [] abilitySkin;
-
-    //Player Ability
-    public enum PlayerAbility
-    {
-        normal,
-        superPower,
-        electricity,
-        magnetic,
-        hacking
-    }
-    private PlayerAbility currentAbility;
-    public void SetPlayerAbility(PlayerAbility a)
-    {
-        currentAbility = a;
-        Debug.Log(currentAbility);
-        if(currentAbility == PlayerAbility.normal)
-            spriteLibrary.spriteLibraryAsset = abilitySkin[0];
-        else if (currentAbility == PlayerAbility.superPower)
-            spriteLibrary.spriteLibraryAsset = abilitySkin[1];
-        else if (currentAbility == PlayerAbility.electricity)
-            spriteLibrary.spriteLibraryAsset = abilitySkin[2];
-        else if (currentAbility == PlayerAbility.magnetic)
-            spriteLibrary.spriteLibraryAsset = abilitySkin[3];
-        else if (currentAbility == PlayerAbility.hacking)
-            spriteLibrary.spriteLibraryAsset = abilitySkin[4];
-    }
-    public PlayerAbility GetPlayerAbility()
-    {
-        return currentAbility;
-    }
-
-    //Player Emotion
-    public enum PlayerEmotion
-    {
-        fine,
-        glad,
-        sad,
-        joy,
-        angry
-    }
-    private PlayerEmotion currentEmotion;
-
-    public void SetPlayerEmotion(PlayerEmotion e)
-    {
-        currentEmotion = e;
-        Debug.Log(currentEmotion);
-    }
-    public PlayerEmotion GetPlayerEmotion()
-    {
-        return currentEmotion;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        uiManager = FindObjectOfType<UIManager>();
-        spriteLibrary = GetComponent<SpriteLibrary>();
-        SetPlayerAbility(PlayerAbility.normal);
     }
 
     IEnumerator MoveCoroutine()
@@ -82,15 +21,9 @@ public class Player : MonoBehaviour
        while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
             if (Input.GetKey(KeyCode.LeftShift)) //´Þ¸®±â
-            {
-                animator.SetBool("Running", true);
                 runSpeed = speed * 0.5f;
-            }
             else
-            {
                 runSpeed = 0;
-                animator.SetBool("Running", false);
-            }
 
             verticalInput = Input.GetAxisRaw("Vertical");
             horizonInput = Input.GetAxisRaw("Horizontal");
@@ -110,14 +43,13 @@ public class Player : MonoBehaviour
         }
 
         animator.SetBool("Walking", false);
-        animator.SetBool("Running", false);
         keyDown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!keyDown && !uiManager.isActiveUI)
+        if(!keyDown)
         {
             
             if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
@@ -127,17 +59,5 @@ public class Player : MonoBehaviour
                 StartCoroutine(MoveCoroutine());
             }
         }
-
-        if (Input.GetKey(KeyCode.Escape) && currentAbility != PlayerAbility.normal)
-            SetPlayerAbility(PlayerAbility.normal);
-
-        if (Input.GetKey(KeyCode.Alpha1))
-            SetPlayerAbility(PlayerAbility.superPower);
-        else if (Input.GetKey(KeyCode.Alpha2))
-            SetPlayerAbility(PlayerAbility.electricity);
-        else if (Input.GetKey(KeyCode.Alpha3))
-            SetPlayerAbility(PlayerAbility.magnetic);
-        else if (Input.GetKey(KeyCode.Alpha4))
-            SetPlayerAbility(PlayerAbility.hacking);
     }
 }
