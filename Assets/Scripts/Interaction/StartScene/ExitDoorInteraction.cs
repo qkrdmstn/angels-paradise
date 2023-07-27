bfa3f166f76b502d7c00e3cf348ea92d579f6085
qkrdmstn <qkrdmstn1258@naver.com>
2023-07-27 12:50:27 +0900

    나가는 문 이벤트 추가

---
+++ Assets/Scripts/Interaction/StartScene/ExitDoorInteraction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoorInteraction : Interaction
+{
+    Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindObjectOfType<Inventory>();
+    }
+    public override InteractionEvent GetEvent()
+    {
+        if (inventory.SearchInventory("액자") != 0 && inventory.SearchInventory("안젤라의 개발일지") != 0)
+            return Events[0];
+        else
+            return Events[1];
+
+    }
+}
