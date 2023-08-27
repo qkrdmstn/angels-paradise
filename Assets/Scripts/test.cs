5c32a78e1c67f912ff0a0ae518789847ac7a521c
qkrdmstn <qkrdmstn1258@naver.com>
2023-08-27 20:22:35 +0900

    CSV 파싱 따옴표 예외처리 추가

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 public class test : MonoBehaviour
 {
-    public string inputText;
-    public string outputText;
+    public string[] inputText;
+    public string[] outputText;
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < inputText.Length; i++)
+        {
+            int last = inputText[i].Length - 1;
 
-        //inputText.Replace("\n", "<br>");
+            if (inputText[i][last] == '\"' && inputText[i][0] == '\"') //처음과 끝 시작이 ""라면 그거 제거 -> 2x+1=t, x=(t-1)/2
+            {
+                inputText[i] = inputText[i].Remove(last, 1);
+                inputText[i] = inputText[i].Remove(0, 1);
 
-        outputText = inputText.Replace("\\n", "\n");
-        this.GetComponent<Text>().text = inputText;
-        //Debug.Log(this.GetComponent<Text>().text);
-        //ttext = this.GetComponent<Text>().text;
+            }
+            outputText[i] = inputText[i].Replace("\"\"", "\""); //2배 된 거 제거
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //outputText = inputText.Replace("\"\"", "\"");
     }
 }
